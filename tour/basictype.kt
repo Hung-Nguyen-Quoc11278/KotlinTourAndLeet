4537b11fa3af121600b0fd26ae0146d9973e8aac
Hung-Nguyen-Quoc11278 <hunggoodboy34@gmail.com>
2026-06-27 15:45:44 +0700

    feat: implement main function with customer variable manipulations

---
+++ tour/basictype.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var customers = 10
+    println("1. $customers")
+    customers = 8
+    println("2. $customers")
+    customers = customers + 3
+    println("3. $customers")
+    customers +=7
+    println("4. $customers")
+    customers -=2
+    println("5. $customers")
+    customers *=3
+    println("6. $customers")
+    customers /=2
+    println("7. $customers")
+}
