4dfc6cffd05e3fa71d6f73ca3369447cae535dbf
Hung-Nguyen-Quoc11278 <hunggoodboy34@gmail.com>
2026-06-27 16:00:27 +0700

    feat: refactor main function and add additional type demonstration functions

--- tour/basictype.kt
+++ tour/basictype.kt
@@ -1,4 +1,4 @@
-fun main(){
+fun customers(){
     var customers = 10
     println("1. $customers")
     customers = 8
@@ -13,4 +13,24 @@ fun main(){
     println("6. $customers")
     customers /=2
     println("7. $customers")
+}
+fun basictype(){
+    val year: Int = 2026 //Byte, Short, Int, Long
+    val score: UInt = 100u //UByte, UShort, UInt, ULong
+    val currentTemp: Float = 24.5f
+    val price: Double = 19.99 //Float, Double
+    val isEnable: Boolean = true //Boolean
+    val separator: Char = ','//Char
+    val message: String = "Hello!"// String
+    println("$year $score $currentTemp $isEnable $separator $message")
+}
+fun declaredType(){
+    val d: Int
+    d = 10
+    print("declared $d")
+}
+fun main(){
+    customers()
+    basictype()
+    declaredType()
 }
