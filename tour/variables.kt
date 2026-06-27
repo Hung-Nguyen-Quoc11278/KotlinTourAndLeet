1434334cb7851e298d885273fd25fb00cdc06ddc
Hung-Nguyen-Quoc11278 <hunggoodboy34@gmail.com>
2026-06-27 15:26:57 +0700

    feat: add initial implementation of main function with variables

---
+++ tour/variables.kt
@@ -0,0 +1,12 @@
+
+
+fun main(){
+    val popcorn = 5
+    val hotdog = 3
+    var customers = 10
+
+    customers = 2
+    println("Popcorn: $popcorn")
+    println("Hotdog: $hotdog")
+    println("Customers: $customers")
+}
